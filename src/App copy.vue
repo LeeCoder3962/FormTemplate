be6a838d10befa823d1536860a76c54278edbd23
LeeCoder3962 <396214291@qq.com>
2020-08-16 16:06:43 +0800

    完成表单页面

--- src/App copy.vue
+++ src/App copy.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    <FormTemplate />
+    <elupload />
   </div>
 </template>
 
 <script>
-import FormTemplate from './components/FormTemplate.vue'
+import elupload from './components/elupload.vue'
 
 export default {
   name: 'App',
   components: {
-    FormTemplate
+    elupload
   }
 }
 </script>
 
 <style>
 #app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px; */
+  margin-top: 60px;
 }
 </style>
